0ace8d49db6528e442fb30f2c53d4d4f1c20292c
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-11-13 03:06:10 -0500

    1 file modified

--- src/app/my-resume/my-resume.component.ts
+++ src/app/my-resume/my-resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-my-resume',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyResumeComponent implements OnInit {
 
-  constructor() { }
+  shippingCosts; 
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
