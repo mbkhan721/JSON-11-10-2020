7c62da41fb1b39d6a2b1fd8a75986d54eef12750
mbkhan721 <muhammad.khan33@student.qcc.cuny.edu>
2020-11-18 02:42:15 -0500

    4 files modified

--- src/app/my-resume/my-resume.component.ts
+++ src/app/my-resume/my-resume.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
 import { r1 } from '../products';
 
 @Component({
@@ -7,11 +6,11 @@ import { r1 } from '../products';
   templateUrl: './my-resume.component.html',
   styleUrls: ['./my-resume.component.css']
 })
-
- export class MyResumeComponent implements OnInit {
+export class MyResumeComponent implements OnInit {
   profile = r1.profile;
-  workExpeience = r1.profile.workExperience;
-  education = r1.profile.education;
+  workExpeience = r1.workExperience ;
+  education = r1.education ;
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/my-resume/resume-work-experience/resume-work-experience.component.ts
+++ src/app/my-resume/resume-work-experience/resume-work-experience.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core' ;
 
+
 @Component({
   selector: 'app-resume-work-experience',
   templateUrl: './resume-work-experience.component.html',
   styleUrls: ['./resume-work-experience.component.css']
 })
 export class ResumeWorkExperienceComponent implements OnInit {
-
- @Input() workExperience ;
- 
+  
+  @Input() profile ;
 
   constructor() { }
 
